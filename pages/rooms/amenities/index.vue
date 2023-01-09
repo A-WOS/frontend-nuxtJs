e461800a93dca9a3fa39870ca3072f132c6c2f13
AWOS <uws0331@gmail.com>
2023-01-09 22:28:26 +0900

    feat: 보여질 amenity 리스트 디자인 및 샘플 데이터 추가

---
+++ pages/rooms/amenities/index.vue
@@ -0,0 +1,69 @@
+<template>
+  <main class="container mt-5">
+    <div class="row">
+      <div class="col-12 text-right mb-4">
+        <div class="d-flex justify-content-between">
+          <h3>Amenities</h3>
+          <nuxt-link to="/rooms/amenities/add" class="btn btn-info">Add Amenity</nuxt-link>
+        </div>
+      </div>
+      <template v-for="amenity in amenities">
+        <div :key="amenity.id" class="col-lg-3 col-md-4 col-sm-6 mb-4">
+          <amenity-card :onDelete="deleteAmenity" :amenity="amenity"></amenity-card>
+        </div>
+      </template>
+    </div>
+  </main>
+</template>
+
+<script>
+import AmenityCard from "@/components/AmenityCard.vue";
+
+const sampleData = [
+  {
+    id: 1,
+    name: "Shower",
+    description: "two shower booths",
+  },
+  {
+    id: 2,
+    name: "kitchen",
+    description: "cook",
+  },
+  {
+    id: 3,
+    name: "amazing rooms",
+    description: "Three rooms"
+  }
+];
+
+export default {
+  head() {
+    return {
+      title: "Amenity list"
+    };
+  },
+  components: {
+    AmenityCard
+  },
+  asyncData(context) {
+    let data = sampleData;
+    return {
+      amenities: data
+    };
+  },
+  data() {
+    return {
+      amenities: []
+    };
+  },
+  methods: {
+    deleteAmenity(amenity_id) {
+      console.log(deleted `${amenity.id}`)
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
