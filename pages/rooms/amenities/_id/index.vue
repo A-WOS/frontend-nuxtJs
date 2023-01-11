0078a014addabffd4c54d2321164dada3e410117
AWOS <uws0331@gmail.com>
2023-01-11 23:44:27 +0900

    feat: amenity 상세 보기 기능 추가

---
+++ pages/rooms/amenities/_id/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <main class="container my-5">
+    <div class="row">
+      <div class="col-12 text-center my-3">
+        <h2 class="mb-3 display-4 text-uppercase">{{ amenity.name }}</h2>
+      </div>
+      <div class="col-md-6">
+        <div class="recipe-details">
+          <h4>Description</h4>
+          <p>{{ amenity.description }}</p>
+        </div>
+      </div>
+      <div>
+        <nuxt-link class="btn btn-outline btn-large btn-info" to="/rooms/amenities">뒤로가기</nuxt-link>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "index",
+  head() {
+    return {
+      title: "View Amenity"
+    };
+  },
+  async asyncData({ $axios, params }) {
+    try {
+      console.log(params)
+      let amenity = await $axios.$get(`/rooms/amenities/${params.id}`);
+      return { amenity };
+    } catch (e) {
+      return { amenity: [] };
+    }
+  },
+  data() {
+    return {
+      amenity: {
+        name: "",
+        description: "",
+      }
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>
